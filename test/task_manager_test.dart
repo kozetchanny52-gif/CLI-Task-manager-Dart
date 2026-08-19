dbef0e38157fa143679ef7e42026b2bcc8213c76
KT <kozetchanny52@gmail.com>
2026-08-19 12:18:18 +0300

    Final commit

--- test/task_manager_test.dart
+++ test/task_manager_test.dart
@@ -1,11 +1,22 @@
-import 'package:task_manager/task_manager.dart';
 import 'package:test/test.dart';
+import 'package:task_manager/task.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+  test('Task is created correctly', () {
+    final task = Task(
+      Type.normal,
+      'Learn Dart testing',
+      DateTime(2026, 8, 19),
+      Priority.high,
+      DateTime(2026, 8, 25),
+      Status.pending,
+    );
 
-calculate() {
+    expect(task.title, 'Learn Dart testing');
+    expect(task.type, Type.normal);
+    expect(task.priority, Priority.high);
+    expect(task.status, Status.pending);
+    expect(task.is_created, DateTime(2026, 8, 19));
+    expect(task.deadline, DateTime(2026, 8, 25));
+  });
 }
